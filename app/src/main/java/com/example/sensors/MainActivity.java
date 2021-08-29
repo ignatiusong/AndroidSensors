2be43075f313110daa68ed0769602334f017a980
ignatiusong <e0324134@u.nus.edu>
2021-08-29 23:26:45 +0800

    Add functionality to get GPS location after getting permissions

--- app/src/main/java/com/example/sensors/MainActivity.java
+++ app/src/main/java/com/example/sensors/MainActivity.java
@@ -3,30 +3,46 @@ package com.example.sensors;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MainActivity extends AppCompatActivity {
+    // UI Elements
+    private Button updateGPSbutton;
+    private TextView gpsTextView;
+
+    // Get permissions to use location
+
     private String[] requiredPermissions = {
-            Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+            Manifest.permission.ACCESS_FINE_LOCATION};//, Manifest.permission.ACCESS_COARSE_LOCATION};
 
     // Register the permissions callback, which handles the user's response to the
     // system permissions dialog. Save the return value, an instance of
     // ActivityResultLauncher, as an instance variable.
     private ActivityResultLauncher<String[]> requestMultiplePermissionLauncher =
             registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(), isGranted -> {
-                if (isGranted.containsValue(true)) {
-                    // Permission is granted. Continue the action or workflow in your
-                    // app.
-                } else {
+                if (isGranted.containsValue(false)) {
                     // Explain to the user that the feature is unavailable because the
                     // features requires a permission that the user has denied. At the
                     // same time, respect the user's decision. Don't link to system
                     // settings in an effort to convince the user to change their
                     // decision.
+                } else {
+                    // Permission is granted. Continue the action or workflow in your
+                    // app.
                 }
             });
 
@@ -36,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
                 this, Manifest.permission.ACCESS_FINE_LOCATION) +
                 ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) ==
                 PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "Have all permissions", Toast.LENGTH_SHORT).show();
             // You can use the API that requires the permission.
             //} else if (shouldShowRequestPermissionRationale(...)) {
             // In an educational UI, explain to the user why your app requires this
@@ -51,12 +68,55 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    // Things to actually get location
+
+    private FusedLocationProviderClient fusedLocationProviderClient;
+
+    private void getLocation() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return ;
+        }
+
+
+        fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                Toast.makeText(MainActivity.this, "Successfully gotten location",
+                        Toast.LENGTH_SHORT).show();
+                gpsTextView.setText("Latitude :" + location.getLatitude() + "Longitude :" + location.getLongitude());
+
+            }
+        });
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Get UI elements
+        updateGPSbutton = findViewById(R.id.updateGPSbutton);
+        gpsTextView = findViewById(R.id.gpsTextView);
+
+        // Check and request for permissions if necessary
         checkMultiplePermissions();
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        updateGPSbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getLocation();
+            }
+        });
+
 
 
 
