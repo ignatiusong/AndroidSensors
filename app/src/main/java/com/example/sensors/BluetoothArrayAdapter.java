aae062ad8a99c41b68ea70958afa3cf1e454f342
ignatiusong <e0324134@u.nus.edu>
2021-09-22 16:29:50 +0800

    Add missing files from previous commit

---
+++ app/src/main/java/com/example/sensors/BluetoothArrayAdapter.java
@@ -0,0 +1,50 @@
+package com.example.sensors;
+
+import android.bluetooth.BluetoothDevice;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BluetoothArrayAdapter extends ArrayAdapter<BluetoothDevice> {
+    private Context mContext;
+    private List<BluetoothDevice> bluetoothDeviceList = new ArrayList<>();
+    private int layoutID;
+
+    public BluetoothArrayAdapter(@NonNull Context context, int resource, @NonNull List<BluetoothDevice> objects) {
+        super(context, resource , objects);
+        mContext = context;
+        bluetoothDeviceList = objects;
+        layoutID = resource;
+
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View listItem = convertView;
+        if(listItem == null)
+            listItem = LayoutInflater.from(mContext).inflate(layoutID,
+                    parent,false);
+
+        BluetoothDevice currentDevice = bluetoothDeviceList.get(position);
+
+        TextView textView = (TextView) listItem.findViewById(android.R.id.text1);
+        if (currentDevice.getName() != null) {
+            textView.setText(currentDevice.getName());
+        } else {
+            textView.setText(currentDevice.toString());
+        }
+
+        return listItem;
+
+    }
+}
